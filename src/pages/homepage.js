197958fd11e8fcfa3377e8853e263fe2233d50c0
Pooja Sammeta <poojasammeta666@gmail.com>
2024-07-15 18:02:54 +0530

    modified files

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -54,7 +54,7 @@ const Homepage = () => {
             <ul className="nav-list">
               <li>
                 <a
-                  href="http://localhost:3000/login"
+                  href="https://server-deployment-sigma.vercel.app/login"
                   style={{ color: "maroon" }}
                 >
                   Logout
